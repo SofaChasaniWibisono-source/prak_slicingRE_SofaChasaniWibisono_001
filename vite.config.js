5941ca614c6e90d39090aceff5e541d6dfe9c52e
sofachaasaniwibisono <sofachaasaniwibisono@gmail.com>
2025-12-07 21:25:52 +0700

    fix github pages base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/prak_slicingRE_SofaChasaniWibisono_001/',
   plugins: [react(), 
     tailwindcss()],
 })
